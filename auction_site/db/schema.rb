d88d2320e49c536da2ad15d40d2571ed06b692a4
Maria <unamaria@gmail.com>
2015-09-05 10:19:08 -0500

    Add index to users email

--- auction_site/db/schema.rb
+++ auction_site/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722155230) do
+ActiveRecord::Schema.define(version: 20150905151758) do
 
   create_table "bids", force: :cascade do |t|
     t.integer  "amount"
@@ -46,5 +46,7 @@ ActiveRecord::Schema.define(version: 20150722155230) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
